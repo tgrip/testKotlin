ce561640a68cc17eb04ad852647c7a3b27e84e47
Theodor Grip <theodor.grip@abas.de>
2021-08-14 13:13:13 +0200

    calculate area

--- src/main/kotlin/ReadDouble.kt
+++ src/main/kotlin/ReadDouble.kt
@@ -1,11 +1,50 @@
+import java.util.*
+import kotlin.math.sqrt
+
 fun main() {
-    val scanner = java.util.Scanner(System.`in`)
+    val scanner = Scanner(System.`in`)
     if (scanner.hasNextLine()) {
         when (scanner.nextLine()) {
-            "Rectangle" -> {
-                println("Rect")
-            }
+            "rectangle" -> printRectangleArea(scanner)
+            "circle" -> printCircleArea(scanner)
+            "triangle" -> printTriangleArea(scanner)
         }
 
+    }
+}
+
+fun printTriangleArea(scanner: Scanner) {
+    var a = 0.0
+    if (scanner.hasNextDouble()) {
+        a = scanner.nextDouble()
+    }
+    var b = 0.0
+    if (scanner.hasNextDouble()) {
+        b = scanner.nextDouble()
+    }
+    var c = 0.0
+    if (scanner.hasNextDouble()) {
+        c = scanner.nextDouble()
+    }
+    val s = (a + b + c) / 2
+    val area = sqrt(s * (s - a) * (s - b) * (s - c))
+    println(area)
+}
+
+fun printCircleArea(scanner: Scanner) {
+    val pi = 3.14
+    if (scanner.hasNextInt()) {
+        val r = scanner.nextInt()
+        println(pi * r * r)
+    }
+}
+
+fun printRectangleArea(scanner: Scanner) {
+    var x = 1.0
+    if (scanner.hasNextDouble()) {
+        x = scanner.nextDouble()
+    }
+    if (scanner.hasNextDouble()) {
+        println(x * scanner.nextDouble())
     }
 }
